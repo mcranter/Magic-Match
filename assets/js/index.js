1b1eb54f2f7ee5732f5b94e93e091995e09ef9bc
mcranter <mcranter@gmail.com>
2020-09-10 11:09:33 +0000

    Cards created

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,67 @@
-console.log("hello");
+document.addEventListener('DOMContentLoaded',() => {
+//Card options 
+const cardArray =[
+    {
+        name:'wand',
+        img: 'images/wand.png'
+    },
+    {
+        name:'wand',
+        img: 'images/wand.png'
+    },
+    {
+        name:'dark',
+        img: 'images/dark-mark.png'
+    },
+    {
+        name:'dark',
+        img: 'images/dark-mark.png'
+    },
+    {
+        name:'dobby',
+        img: 'images/dobby-red.png'
+    },
+    {
+        name:'dobby',
+        img: 'images/dobby-red.png'
+    },
+    {
+        name:'hallow',
+        img: 'images/hallow.png'
+    },
+    {
+        name:'hallow',
+        img: 'images/hallow.png'
+    },
+    {
+        name:'hedwig',
+        img: 'images/hedwig-white.png'
+    },
+    {
+        name:'hedwig',
+        img: 'images/hedwig-white.png'
+    },
+    {
+        name:'harry',
+        img: 'images/hp.png'
+    },
+    {
+        name:'harry',
+        img: 'images/hp.png'
+    }
+]
 
-/* removed 'hidden' class from card*/
-function onCardClicked(e) {
-    const target = e.currentTarget;
-    console.log(target.className);
-    target.className = target.className.replace('color-hidden', '').trim();
+const grid = document.querySelector.('.grid')
+
+//creates board //
+
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        var card = document.createElement('img')
+    }
 }
 
+
+
+
+})
