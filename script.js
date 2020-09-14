4106eeebceb0cfbe2798259411c54f9caed0e68e
mcranter <mcranter@gmail.com>
2020-09-14 21:18:01 +0000

    Edited card flip function

--- script.js
+++ script.js
@@ -4,5 +4,18 @@ function flipCard(){
     this.classList.toggle('flip');
 }
 
+if (!hasFlippedCard){
+    //first click
+    hasFlippedCard = true;
+    firstCard = this;
+}   else    {
+    hasFlippedCard = false;
+    secondCard = this;
+
+    //check if cards match
+    }
+}
+
 cards.forEach(card => card.addEventListener('click', flipCard))
 ;
+
