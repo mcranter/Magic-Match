9080777c2c8142805eb88d1ad8c373b90334a060
mcranter <mcranter@gmail.com>
2020-09-15 09:06:45 +0000

    Storing and matching functions written

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard(){
-    this.classList.toggle('flip');
-}
+    this.classList.add('flip');
 
-if (!hasFlippedCard){
-    //first click
-    hasFlippedCard = true;
-    firstCard = this;
-}   else    {
-    hasFlippedCard = false;
-    secondCard = this;
+    if (!hasFlippedCard){
+        //first click
+        hasFlippedCard = true;
+        firstCard = this;
+    }   else    {
+        //second click
+        hasFlippedCard = false;
+        secondCard = this;
 
-    //check if cards match
+        //check if cards match
+        if (firstCard.dataset.image ===
+            secondCard.dataset.image){
+            //if it matches
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);    
+            //if there's no match
+            } else {
+                setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+            },  1500); //timer needed to show card flip 
+        }
     }
 }
-
 cards.forEach(card => card.addEventListener('click', flipCard))
 ;
 
