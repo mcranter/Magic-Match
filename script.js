1dcc52e3720d94773c33e3188e60fe7c7e68c59e
mcranter <mcranter@gmail.com>
2020-09-15 09:25:48 +0000

    Refactored code

--- script.js
+++ script.js
@@ -10,26 +10,38 @@ function flipCard(){
         //first click
         hasFlippedCard = true;
         firstCard = this;
-    }   else    {
-        //second click
-        hasFlippedCard = false;
-        secondCard = this;
-
-        //check if cards match
-        if (firstCard.dataset.image ===
-            secondCard.dataset.image){
-            //if it matches
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);    
-            //if there's no match
-            } else {
-                setTimeout(() => {
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            },  1500); //timer needed to show card flip 
-        }
+        
+        return;
     }
+    //second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    checkForMatch();
 }
+
+
+//ternary operator used check if cards match
+function checkForMatch () {
+    let isMatch = firstCard.dataset.image ===
+        secondCard.dataset.image;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+//disables flipping in the event of a match
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard); 
+}
+//unflips cards if there's no match 
+function unflipCards(){
+    setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+        },  1500);
+}
+
 cards.forEach(card => card.addEventListener('click', flipCard))
 ;
 
