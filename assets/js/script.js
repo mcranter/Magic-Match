e66ab1a10f7d3b6ab72de5f3432a74b74d40ec62
mcranter <mcranter@gmail.com>
2020-09-29 20:11:13 +0000

    Menu modal added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,34 @@
 const cards = document.querySelectorAll('.card');
 
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click',() => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+closeModalButtons.forEach(button => {
+    button.addEventListener('click',() => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(model) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
