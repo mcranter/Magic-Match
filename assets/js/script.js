c4d161013c5fa7f7f6402babec4c0c2a340c7e28
mcranter <mcranter@gmail.com>
2020-10-05 12:41:32 +0000

    Removed unnecessary code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,4 @@
 const cards = document.querySelectorAll('.card');
-//Modal controls
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-
-openModalButtons.forEach(button => {
-    button.addEventListener('click',() => {
-        const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
-})
-//closes modal by clicking outside it 
-overlay.addEventListener('click', () => {
-    const modals = document.querySelectorAll('.modal.active')
-    modals.forEach(modal => {
-        closeModal(modal)
-    })
-})
-closeModalButtons.forEach(button => {
-    button.addEventListener('click',() => {
-        const modal = button.closest('.modal')
-        closeModal(modal)
-    })
-})
 
 function openModal(modal) {
     if (modal == null) return
